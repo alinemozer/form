d1c1a52b1cb1b2dcb14dfb1101578d0cac1aa97c
Aline Mozer <alinemozer@gmail.com>
2022-07-05 16:58:04 -0300

    Conclusão do JS.

--- src/js/index.js
+++ src/js/index.js
@@ -3,22 +3,25 @@ const campoObrigatorio = document.querySelectorAll("span");
 const botaoEnviar = document.querySelector(".btn");
 
 camposFormulario.forEach(input => {
-    input.addEventListener('change', () => {
+    input.addEventListener('input', () => {
         if (input.value != "") {
             input.classList.add("campo-preenchido")
-        }else {
+        } else {
             input.classList.remove("campo-preenchido")
         }
     });
 });
 
-botaoEnviar.addEventListener('click', () => {
+botaoEnviar.addEventListener('click', (e) => {
+    e.preventDefault()
     camposFormulario.forEach(input => {
         if (input.value == "") {
-        input.classList.add("erro-validacao")
-    }
+            input.classList.add("erro-validacao");
+            input.nextElementSibling.classList.add("erro-validacao");
+        } else {
+            input.classList.remove("erro-validacao");
+            input.nextElementSibling.classList.remove("erro-validacao");
+        }
     });
 });
-
-
 
