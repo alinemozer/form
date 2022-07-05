48c1f3ffac53159957793d149cd85615d2960fa9
Aline Mozer <alinemozer@gmail.com>
2022-07-05 14:42:47 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const camposFormulario = document.querySelectorAll(".input");
+const campoObrigatorio = document.querySelectorAll("span");
+const botaoEnviar = document.querySelector(".btn");
+
+camposFormulario.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value != "") {
+            input.classList.add("campo-preenchido")
+        }else {
+            input.classList.remove("campo-preenchido")
+        }
+    });
+});
+
+botaoEnviar.addEventListener('click', () => {
+    camposFormulario.forEach(input => {
+        if (input.value == "") {
+        input.classList.add("erro-validacao")
+    }
+    })
+}) 
+
+
+
